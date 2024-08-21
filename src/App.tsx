7710b99499de14068781946f3dd29c84df5bd100
Pedro Barboza <peh123barboza@gmail.com>
2024-08-21 20:15:00 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Home from "./pages/home/home"
+
+function App() {
+  // Código TypeScript
+
+  return (
+    // Código TSX => HTML + CSS
+    <> 
+
+      <Home />
+
+    </>
+  )
+}
+
+export default App
+
