da8c1743568aa5241712d5fb64df72f659503d25
Pedro Barboza <peh123barboza@gmail.com>
2024-08-24 00:18:50 -0300

    Layout Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import Home from "./pages/home/home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/footer"
+import Home from "./pages/home/Home"
+import Navbar from "./components/navbar/navbar"
 
 function App() {
   // Código TypeScript
 
   return (
     // Código TSX => HTML + CSS
-    <> 
-
-      <Home />
-
-    </>
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  </>
   )
 }
 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,44 +1,34 @@
 
 function Home() {
-
-
-
+   
     return (
-        <div style={{
-            width: "100vw",
-            display: "flex",
-            justifyContent: "center"
-        }}>
-            <div>
-                <div style={{
-                    width: "80vw",
-                    display: "flex",
-                    flexDirection: "column", // flex-direction
-                    alignItems: "center", // align-items
-                }}>
-                    <h2>Seja Bem Vinde!</h2>
-                    <p>Expresse aqui seus pensamentos e opiniões</p>
+        <div className="flex justify-center bg-indigo-900 ">
+            <div className="container grid grid-cols-2 text-white ">
+                <div className="flex flex-col items-center justify-center gap-4 py-4 ">
+                    <h2 className="text-5xl font-bold ">
+                        Seja Bem Vinde!
+                    </h2>
+                    <p className="text-xl">
+                        Expresse aqui seus pensamentos e opiniões
+                    </p>
+
+                    <div className="flex justify-around gap-4 ">
+                        <div className="px-4 py-2 border-2 border-white border-solid rounded ">
+                            Nova Postagem
+                        </div>
+                    </div>
+
                 </div>
-                <div style={{
-                    width: "80vw",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}>
+                <div className="flex justify-center ">
                     <img
                         src="https://i.imgur.com/VpwApCU.png"
                         alt="Imagem da Página Home"
-                        width="400px"
+                        className="w-2/3"
                     />
                 </div>
             </div>
-
         </div>
-
-        
     )
 }
 
-export default Home;
-
-/*rfce*/
+export default Home
