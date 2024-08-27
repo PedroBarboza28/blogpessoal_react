d56d4cafd29d2e769601a0bf300639578041c22e
Pedro Barboza <peh123barboza@gmail.com>
2024-08-26 21:25:04 -0300

    Cadastro e Login de Usuário

--- src/pages/cadastro/cadastro.tsx
+++ src/pages/cadastro/cadastro.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import Usuario from '../../models/Usuario'
 import './Cadastro.css'
 import { useNavigate } from 'react-router-dom';
@@ -7,77 +7,88 @@ import { cadastrarUsuario } from '../../services/service';
 
 function Cadastro() {
 
-//hook usenavigate para redirecionar rotas
-const navigate = useNavigate();
+  // Hook useNavigate para redirecionar rotas
+  const navigate = useNavigate();
 
-//estado que vai guardar os dados do meu usuario
-const [usuario, setUsuario] = useState<Usuario>({
-  id: 0,
-  nome: "",
-  usuario: "",
-  senha: "",
-  foto: ""
-})
-
-//estado que vai guardar a confirmaçao da senha
-const [confirmaSenha, setConfirmaSenha] = useState<string>("");
-
-//estado que vai indicar quando a animaçao (loader) será carregada
-const [isLoading, setIsLoading] = useState<boolean>(false);
-
-//useEffect para monitorar o Estado usuario
-useEffect(() => {
-  if (usuario.id !== 0){
-    retornar()
-  }
-}, [usuario])
-
-//redireciona para o componente login (rota /login)
-function retornar(){
-  navigate(`/login`)
-}
-
-//funçao que atualiza as propriedades do Estado usuario
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>){
-  setUsuario({
-    ...usuario,
-    [e.target.name] : e.target.value 
+  // Estado que vai guardar os dados do meu usuário
+  const [usuario, setUsuario] = useState<Usuario>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: ""
   })
-}
 
-function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>){
-  setConfirmaSenha(e.target.value)
-}
+  // Estado que vai guardar a confirmação da senha
+  const [confirmaSenha, setConfirmaSenha] = useState<string>("");
 
-async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
-  e.preventDefault()
+  // Estado que vai indicar quando a animação (loader) será carregada
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  if (confirmaSenha === usuario.senha && usuario.senha.length >= 8){
-    setIsLoading(true)
-
-    try{
-
-      await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuario )
-      alert("Usuário cadastrado com sucesso!")
-      
-    }catch(error){
-      alert('Erro ao cadastrar o usuário!')
+  // useEffect para monitorar o Estado usuario
+  useEffect(() => {
+    if (usuario.id !== 0){
+      retornar()
     }
-  }else{
-    alert("Dados Inconsistentes! Verifique as informações do cadastro.")
-    setUsuario({...usuario, senha: ""})
-    setConfirmaSenha("")
+  }, [usuario])
+
+  // Redireciona para o Componente Login (rota /login)
+  function retornar() {
+    navigate('/login')
   }
 
-  setIsLoading(false)
-} 
+  // Função que atualiza as propriedades do Estado Usuário com os dados digitados no input
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value
+    })
+  }
 
-console.log(usuario)
-console.log(confirmaSenha)
+  // Função que atualiza o Estado confirmaSenha
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmaSenha(e.target.value)
+  }
+
+  // Função para cadastrar um novo usuário
+  async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>) {
+
+    // Impede o comportamento padrão do formulário
+    e.preventDefault()
+
+    // Valida a senha
+    if (confirmaSenha === usuario.senha && usuario.senha.length >= 8) {
+
+      // Inicializa a animação
+      setIsLoading(true)
+
+      try {
+
+        // Executa a função de cadastro do usuário
+        await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuario)
+        alert("Usuário Cadastrado com Sucesso!")
+
+      } catch (error) {
+        alert('Erro ao cadastrar o usuário!')
+      }
+
+    } else {
+      alert("Dados inconsistentes! Verifique as informações do Cadastro.")
+      setUsuario({ ...usuario, senha: "" })
+      setConfirmaSenha("")
+    }
+
+    // Finaliza a animação
+    setIsLoading(false)
+  }
+
+  console.log(usuario)
+  console.log(confirmaSenha)
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen 
+            place-items-center font-bold">
         <div className="fundoCadastro hidden lg:block"></div>
         <form onSubmit={cadastrarNovoUsuario} className='flex justify-center items-center flex-col w-2/3 gap-3' >
           <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
@@ -89,8 +100,8 @@ console.log(confirmaSenha)
               name="nome"
               placeholder="Nome"
               className="border-2 border-slate-700 rounded p-2"
-              value = {usuario.nome}
-              onChange = {(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              value={usuario.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -101,8 +112,8 @@ console.log(confirmaSenha)
               name="usuario"
               placeholder="Usuario"
               className="border-2 border-slate-700 rounded p-2"
-              value = {usuario.usuario}
-              onChange = {(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              value={usuario.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -113,8 +124,8 @@ console.log(confirmaSenha)
               name="foto"
               placeholder="Foto"
               className="border-2 border-slate-700 rounded p-2"
-              value = {usuario.foto}
-              onChange = {(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              value={usuario.foto}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -125,8 +136,8 @@ console.log(confirmaSenha)
               name="senha"
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
-              value = {usuario.senha}
-              onChange = {(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              value={usuario.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -137,16 +148,31 @@ console.log(confirmaSenha)
               name="confirmarSenha"
               placeholder="Confirmar Senha"
               className="border-2 border-slate-700 rounded p-2"
-              value = {confirmaSenha}
-              onChange = {(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+              value={confirmaSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
             />
           </div>
           <div className="flex justify-around w-full gap-8">
-            <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2' >
+            <button className='rounded text-white bg-red-400 
+                  hover:bg-red-700 w-1/2 py-2' >
               Cancelar
             </button>
-            <button className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center' type='submit'>
-            {isLoading ? <RotatingLines strokeColor="white" strokeWidth="5" animationDuration="0.75" width="24" visible={true} /> : <span>Cadastrar</span>}
+            <button
+              type='submit'
+              className='rounded text-white bg-indigo-400 
+                           hover:bg-indigo-900 w-1/2 py-2
+                           flex justify-center'
+            >
+              {isLoading ? <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              /> :
+                <span>Cadastrar</span>
+              }
+
             </button>
           </div>
         </form>
