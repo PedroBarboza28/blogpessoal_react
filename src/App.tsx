b77571320914f936fc4a9792805fa737b02713d2
Pedro Barboza <peh123barboza@gmail.com>
2024-08-29 16:56:22 -0300

    feat: add tema and formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
-import Cadastro from './pages/cadastro/cadastro';
-import Navbar from './components/navbar/navbar';
-import Footer from './components/footer/footer';
-import Login from './pages/login/login';
-
+import { AuthProvider } from './contexts/AuthContext';
+import NavBar from './components/navbar/NavBar';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import FormularioTema from './components/temas/formulariotemas/FormularioTemas';
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formulariopostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
 
 function App() {
   return (
     <>
-   
+    <AuthProvider>
         <BrowserRouter>
-          <Navbar />
+          <NavBar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-  
+        </AuthProvider>
     </>
   );
 }
---
+++ src/components/postagens/modalpostagem/ModalPostagem.tsx
@@ -0,0 +1,19 @@
+import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup';
+import './ModalPostagem.css'
+import FormularioPostagem from '../formulariopostagem/FormularioPostagem';
+
+function ModalPostagem() {
+  return (
+    <>
+      <Popup 
+      trigger={<button className='border rounded px-4 hover:bg-white hover:text-indigo-800'>Nova postagem</button>} modal>
+        <div>
+          <FormularioPostagem />
+        </div>
+      </Popup>
+    </>
+  );
+}
+
+export default ModalPostagem;
